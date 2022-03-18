f8a04162f285299e80beb180688047eca7a7a91c
chris_chan <chrischandfw@gmail.com>
2022-03-18 00:37:15 -0500

    added more styling

--- render-utils.js
+++ render-utils.js
@@ -6,6 +6,7 @@ export function renderGoblin(goblinData) {
 
     goblinEl.classList.add('goblin');
 
+
     nameEl.textContent = goblinData.name;
     hpEl.textContent = goblinData.hp < 0 ? 0 : goblinData.hp;
 
