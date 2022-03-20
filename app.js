9ff82f314e0b06a2a32035128fd16e1efd0e804f
chris_chan <chrischandfw@gmail.com>
2022-03-20 01:15:01 -0500

    grabbed dom and set state

---
+++ app.js
@@ -0,0 +1,7 @@
+const selectEl = document.querySelector = ('select');
+const defeatedMonsterEl = document.querySelector = ('defeated-number');
+
+let defeatedMonsterCount = 0;
+let retainers = [
+    { name: 'Hanuman', }
+];
