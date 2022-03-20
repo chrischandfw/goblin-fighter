a107894d2a19a6763b2d9397df43a301e5ef08d4
chris_chan <chrischandfw@gmail.com>
2022-03-20 12:38:16 -0500

    created data file for retainers

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const defeatedMonsterEl = document.querySelector = ('defeated-number');
 
 let defeatedMonsterCount = 0;
 let retainers = [
-    { name: 'Hanuman', level: 1, }
+    { name: 'Hanuman', level: 1, strength: 5, attack: 10 },
+    { name: 'Naresuan', }
 ];
---
+++ retainersData.js
@@ -0,0 +1,32 @@
+export const retainers = [
+    { 	name: 'Hanuman', 
+        level: 1, 
+        strength: 5, 
+        attack: 10,
+        details: 'Divine monkey god' },
+    { 
+        name: 'Naren', 
+        level: 1, 
+        strength: 3, 
+        attack: 6, 
+        details: 'Souriyan soldier, loyal to Prince Gomin' },
+    { 
+        name: 'Gomin', 
+        level: 1, 
+        strength: 1, 
+        attack: 2,
+        details: 'Banished member of the royal family' },
+    { 
+        name: 'Anothai', 
+        level: 1, 
+        strength: 4, 
+        attack: 8,
+        details: 'Angel disguised as a human' },
+    { 
+        name: 'Bounrak', 
+        level: 1, 
+        strength: 2, 
+        attack: 4,
+        details: 'Military general from Lan Xang kingdom' 	
+    }
+];
