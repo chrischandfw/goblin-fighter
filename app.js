843caad2edab1253e58cedfd9c7a9aae7c5d38d0
chris_chan <chrischandfw@gmail.com>
2022-03-20 20:14:42 -0500

    added loop to dropdown event listener

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import { retainers } from './retainersData';
 
 const selectEl = document.querySelector = ('.rets-dropdown');
 const defeatedMonsterEl = document.querySelector = ('defeated-number');
+const errorElement = document.querySelector('#error-message');
+const list = document.querySelector('#list');
 
 let defeatedMonsterCount = 0;
 let monsters = [
@@ -12,5 +14,11 @@ let monsters = [
 selectEl.addEventListener('change', (e) => {
     const selected = e.target.value;
 	
+    if (selected === 'none') {
+		list.innerHTML = '';
+		errorElement.innerHTML = '';
+		p.textContent = 'PLEASE SELECT A RETAINER';
+		errorElement.appendChild(p);
+    } else if ()
 	
 });
