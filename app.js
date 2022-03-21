fd9e09e6970e623716e909b277628cc77e20b345
chris_chan <chrischandfw@gmail.com>
2022-03-20 20:46:26 -0500

    added another div to hold retainer info

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import { retainers } from './retainersData';
+import { renderRetainers } from './renderRetainers';
 
 const selectEl = document.querySelector = ('.rets-dropdown');
 const defeatedMonsterEl = document.querySelector = ('defeated-number');
@@ -15,10 +16,24 @@ selectEl.addEventListener('change', (e) => {
     const selected = e.target.value;
 	
     if (selected === 'none') {
-		list.innerHTML = '';
-		errorElement.innerHTML = '';
-		p.textContent = 'PLEASE SELECT A RETAINER';
-		errorElement.appendChild(p);
-    } else if ()
+        list.innerHTML = '';
+        errorElement.innerHTML = '';
+        p.textContent = 'PLEASE SELECT A RETAINER';
+        errorElement.appendChild(p);
+    } else if (selected === 'hanuman') {
+        renderRetainers(0);
+    } else if (selected === 'naren') {
+		renderRetainers(1);
+	} else if (selected === 'gomin') {
+		renderRetainers(2);
+	} else if (selected === 'anothai') {
+		renderRetainers(3);
+	} else (selected === 'bounrak') {
+		renderRetainers(4);
+	}
 	
 });
+
+function renderRetainers(index) {
+	
+}
--- render-monsters.js
+++ render-monsters.js
@@ -2,22 +2,6 @@ export function renderMonster(monsters) {
     const monstersDiv = document.createElement('div');
     monstersDiv.classList.add('monstersCard');
 
-    const opponentInfoDiv = document.createElement('div');
-    opponentInfoDiv.classList.add('flex-column', 'flex-start');
 
-    const opponentName = document.createElement('p');
-    opponentName.textContent = `Name: ${opponent.name}`;
-
-    const opponentHealth = document.createElement('p');
-    opponentHealth.textContent = getHealthText(opponent.health);
-    opponentHealth.id = `health${opponent.id}`;
-
-    const opponentImage = document.createElement('img');
-    opponentImage.src = `assets/${opponent.shortname}-small.png`;
-    opponentImage.classList.add('smallImg');
-
-    opponentInfoDiv.append(opponentName, opponentHealth);
-    monstersDiv.append(opponentInfoDiv, opponentImage);
-
-    return opponentCardDiv;
+    return monstersDiv;
 }
